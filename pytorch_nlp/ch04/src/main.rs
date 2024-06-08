2a310a88db842b74aa205a97c94baf0cfd25ce24
kigi <kigi.chang@gmail.com>
2024-06-08 13:40:18 +0800

    2024-06-08 13:40

--- pytorch_nlp/ch04/src/main.rs
+++ pytorch_nlp/ch04/src/main.rs
@@ -3,9 +3,9 @@ fn main() {
 }
 
 #[cfg(test)]
-mod tests {
+mod tests_4_1 {
 
-    use candle_core::{DType, Device, Tensor, D};
+    use candle_core::{DType, Device, IndexOp, Tensor, D};
     use tracing_chrome::ChromeLayerBuilder;
     use tracing_subscriber::prelude::*;
 
@@ -225,5 +225,173 @@ mod tests {
 
         let t3 = t.transpose(1, 2).unwrap();
         println!("transpose(1, 2): {:?}", t3.to_vec3::<f32>().unwrap());
+    }
+
+    #[test]
+    fn test_squeeze() {
+        let t = Tensor::from_iter(1..=6_i64, &Device::Cpu)
+            .unwrap()
+            .reshape((2, 3))
+            .unwrap();
+
+        println!("orginal: {:?}", t.to_vec2::<i64>().unwrap());
+
+        let t2 = t.unsqueeze(0).unwrap();
+        println!(
+            "unsqueeze(0): {:?}, {:?}",
+            t2.to_vec3::<i64>().unwrap(),
+            t2.shape()
+        );
+        let t20 = t.squeeze(0).unwrap();
+        println!(
+            "\tsqueeze(0): {:?}, {:?}",
+            t20.to_vec2::<i64>().unwrap(),
+            t20.shape()
+        );
+
+        let t3 = t.unsqueeze(1).unwrap();
+        println!(
+            "unsqueeze(1): {:?}, {:?}",
+            t3.to_vec3::<i64>().unwrap(),
+            t3.shape()
+        );
+
+        let t31 = t3.squeeze(1).unwrap();
+        println!(
+            "\tsqueeze(1): {:?}, {:?}",
+            t31.to_vec2::<i64>().unwrap(),
+            t31.shape()
+        );
+
+        let t30 = t3.squeeze(0).unwrap();
+        println!(
+            "\tsqueeze(0): {:?}, {:?}",
+            t30.to_vec3::<i64>().unwrap(),
+            t30.shape()
+        );
+
+        let t4 = t.unsqueeze(2).unwrap();
+        println!(
+            "unsqueeze(2): {:?}, {:?}",
+            t4.to_vec3::<i64>().unwrap(),
+            t4.shape()
+        );
+
+        let t42 = t4.squeeze(D::Minus1).unwrap();
+        println!(
+            "\tsqueeze(D::Minus1): {:?}, {:?}",
+            t42.to_vec2::<i64>().unwrap(),
+            t42.shape()
+        );
+
+        let t42 = t4.squeeze(2).unwrap();
+        println!(
+            "\tsqueeze(2): {:?}, {:?}",
+            t42.to_vec2::<i64>().unwrap(),
+            t42.shape()
+        );
+
+        let t41 = t4.squeeze(1).unwrap();
+        println!(
+            "\tsqueeze(1): {:?}, {:?}",
+            t41.to_vec3::<i64>().unwrap(),
+            t41.shape()
+        );
+    }
+
+    #[test]
+    fn test_expand() {
+        let x = Tensor::new(&[[[0.5_f32, 0.1, 0.3]], [[0.8, 0.2, 0.1]]], &Device::Cpu).unwrap();
+        println!("x:{:?}, {:?}", x.to_vec3::<f32>().unwrap(), x.shape());
+
+        let y = x.expand((2, 8, 3)).unwrap();
+        println!("y:{:?}, {:?}", y.to_vec3::<f32>().unwrap(), y.shape());
+    }
+
+    #[test]
+    fn test_repeat() {
+        let x = Tensor::new(&[[[0.5_f32, 0.1, 0.3]], [[0.8, 0.2, 0.1]]], &Device::Cpu).unwrap();
+        println!("x:{:?}, {:?}", x.to_vec3::<f32>().unwrap(), x.shape());
+
+        let y = x.repeat((2, 2, 2)).unwrap();
+        println!("y:{:?}, {:?}", y.to_vec3::<f32>().unwrap(), y.shape());
+
+        let y = x.repeat((1, 2, 3)).unwrap();
+        println!("y:{:?}, {:?}", y.to_vec3::<f32>().unwrap(), y.shape());
+    }
+
+    #[test]
+    fn test_index() {
+        let t = Tensor::from_iter(1..=6_i64, &Device::Cpu)
+            .unwrap()
+            .reshape((2, 3))
+            .unwrap();
+
+        println!("{:?}", t.to_vec2::<i64>().unwrap());
+
+        let t1 = t.i(1).unwrap();
+        println!("{:?}", t1.to_vec1::<i64>().unwrap());
+
+        let t2 = t.i((1, 2)).unwrap();
+        println!("{:?}", t2.to_scalar::<i64>().unwrap());
+
+        let t = Tensor::from_iter(1..=9_i64, &Device::Cpu)
+            .unwrap()
+            .reshape((3, 3))
+            .unwrap();
+
+        let t1 = t.i((.., 1)).unwrap();
+        println!("{:?}", t1.to_vec1::<i64>().unwrap());
+    }
+}
+
+#[cfg(test)]
+mod tests_4_2 {
+
+    use candle_core::{DType, Device, Tensor};
+
+    #[test]
+    fn test_add() {
+        let t1 = Tensor::arange(1_i64, 4_i64, &Device::Cpu).unwrap();
+        println!("t1: {:?}", t1.to_vec1::<i64>().unwrap());
+        let t2 = Tensor::arange(4_i64, 7_i64, &Device::Cpu).unwrap();
+        println!("t2: {:?}", t2.to_vec1::<i64>().unwrap());
+
+        let t3 = t1.add(&t2).unwrap();
+        println!("t3: {:?}", t3.to_vec1::<i64>().unwrap());
+
+        let t3 = (&t1 + &t2).unwrap(); // 要用 `&` 否則會被 move
+        println!("t3: {:?}", t3.to_vec1::<i64>().unwrap());
+
+        let t3 = t1.broadcast_add(&t2).unwrap();
+        println!("t3: {:?}", t3.to_vec1::<i64>().unwrap());
+
+        let one = Tensor::ones(1, DType::I64, &Device::Cpu).unwrap();
+        println!("one: {:?}", one.to_vec1::<i64>().unwrap());
+
+        let t3 = t1.broadcast_add(&one).unwrap();
+        println!("t3: {:?}", t3.to_vec1::<i64>().unwrap());
+
+        // let t3 = (&t1 + &one).unwrap(); // error
+        // let t3 = t1.add(&one).unwrap(); // error
+
+        let t1 = Tensor::from_iter(1..=3_i64, &Device::Cpu).unwrap();
+        let t2 = Tensor::from_iter(1..=9_i64, &Device::Cpu)
+            .unwrap()
+            .reshape((3, 3))
+            .unwrap();
+
+        println!("t1: {:?}", t1.to_vec1::<i64>().unwrap());
+        println!("t2: {:?}", t2.to_vec2::<i64>().unwrap());
+
+        let t3 = t1.broadcast_add(&t2).unwrap();
+        println!("t3: {:?}", t3.to_vec2::<i64>().unwrap());
+
+        let t3 = t2.broadcast_add(&t1).unwrap();
+        println!("t3: {:?}", t3.to_vec2::<i64>().unwrap());
+
+        // let t1 = Tensor::from_iter(1..=3_i64, &Device::Cpu).unwrap();
+        // let t2 = Tensor::from_iter(1..=9_i64, &Device::Cpu).unwrap();
+        // let t3 = t1.broadcast_add(&t2).unwrap(); // error
     }
 }
